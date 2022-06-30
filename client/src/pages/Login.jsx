95e02aceb51ab7eade5a037926381e4b562b2f61
dvlptuankiendo <dvlptuankiendo@gmail.com>
2022-06-30 17:44:49 +0700

    feat(UI): login page

---
+++ client/src/pages/Login.jsx
@@ -0,0 +1,20 @@
+const fbImageUrl = `https://vi.wikipedia.org/wiki/T%E1%BA%ADp_tin:Facebook-icon-1.png`;
+const googleImageUrl = `https://phukienbaominh.com/google-icon`;
+
+const Login = () => {
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <p>Facebook</p>
+      <p>Google</p>
+    </div>
+  );
+};
+
+export default Login;
